e8e27fcdc8b7dd5caad99b6143566eb7d06e466b
Alex Johnson <johnsonav1992@gmail.com>
2024-04-25 20:13:33 -0500

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './components/about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about'
+    , component: AboutPageComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
